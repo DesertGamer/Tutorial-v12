10b03a3187bdf89f159ff29faa75c652f48b42f8
Desert Cave Development <44316028+DesertCaveDevelopment@users.noreply.github.com>
2020-07-06 14:42:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const botconfig = require("../botconfig.json");
 const moment = require("moment");
 const ms = require('parse-ms');
 
-bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
-cooldowns = new discord.Collection();
-
-bot.categories = fs.readdirSync("./commands/");
-
-["command"].forEach(handler => {require(`./DcdHandler/${handler}`)(bot);});
-
 bot.on("ready", async() => {
   const channel = bot.channels.cache.get(`701667050694639727`);
   if (channel) {
@@ -77,17 +69,6 @@ bot.on("message", async (message) => {
       await msg.react("👍");
       await msg.react("👎");
     });
-  }
-  let prefix = `/`;
-  if (!message.content.startsWith(prefix)) return;
-  if (!message.member) message.member = await message.guild.fetchMember(message);
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length === 0) return;
-  let command = bot.commands.get(cmd);
-  if (!command) command = bot.commands.get(bot.aliases.get(cmd));
-  if (command) {
-    command.run(bot, message, args);
   }
 });
 
